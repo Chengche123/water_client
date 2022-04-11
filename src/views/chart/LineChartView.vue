7c8f6317c0f9458ec95d07e56a67e810be721c41
佘曾伟 <chengche245@163.com>
2022-04-11 10:41:37 +0800

    feat(views): add LineChartView

---
+++ src/views/chart/LineChartView.vue
@@ -0,0 +1,112 @@
+<template>
+  <LineChart :labels="labels" :datasets="datasets" />
+  <div class="text-center">
+    <button @click="handleClick">测试</button>
+  </div>
+  <!-- <div>{{ msgData.code == code ? msgData : "" }}</div> -->
+</template>
+
+<script>
+import LineChart from "../../components/LineChart";
+import { CHART_COLORS, transparentize } from "../../utils/utils";
+import axios from "axios";
+
+function parseUdatetime(udatetime) {
+  const date = new Date(udatetime);
+  const datetime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  return datetime;
+}
+
+export default {
+  name: "LineChartView",
+  components: {
+    LineChart,
+  },
+  props: {
+    msgData: {
+      type: Object,
+    },
+    code: {
+      type: Number,
+    },
+    dataPath: {
+      type: String,
+    },
+    sensorPath: {
+      type: String,
+    },
+  },
+  async mounted() {
+    // 先获取一部分数据显示在图表上
+    try {
+      const response = await axios.get(this.dataPath, {
+        params: {
+          code: this.code,
+          limit: 10,
+          offset: 0,
+        },
+      });
+      const results = response.data.results;
+      results.reverse();
+      let lables = [];
+      let datasetsData = [];
+      for (let { udatetime, value } of results) {
+        const datetime = parseUdatetime(udatetime);
+        lables.push(datetime);
+        datasetsData.push(value);
+      }
+      this.labels = lables;
+      this.datasets[0].data = datasetsData;
+    } catch (error) {
+      console.log(error);
+    }
+
+    // 获取传感器信息
+    try {
+      const response = await axios.get(this.sensorPath, {
+        params: {
+          code: this.code,
+        },
+      });
+      const sensorInfo = response.data?.results?.[0];
+      if (sensorInfo) {
+        this.datasets[0].label = `${sensorInfo.address} - ${sensorInfo.sensortypename}`;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  watch: {
+    msgData: function (val) {
+      if (!val?.value) {
+        return;
+      }
+      this.labels = this.labels.slice(1).concat(parseUdatetime(val.udatetime));
+      this.datasets[0].data = this.datasets[0].data.slice(1).concat(val.value);
+      console.log(val.value);
+    },
+  },
+  data() {
+    return {
+      labels: [],
+      datasets: [
+        {
+          label: "温度",
+          data: [],
+          borderColor: CHART_COLORS.red,
+          backgroundColor: transparentize(CHART_COLORS.red, 0.5),
+          pointStyle: "circle",
+          pointRadius: 5,
+          pointHoverRadius: 10,
+        },
+      ],
+    };
+  },
+  methods: {
+    handleClick() {
+      this.labels = this.labels.slice(1).concat("Foo");
+      this.datasets[0].data = this.datasets[0].data.slice(1).concat(20);
+    },
+  },
+};
+</script>
