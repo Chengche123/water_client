abfb8e837c841f8db2b643c5e7486630b3658fe6
佘曾伟 <chengche245@163.com>
2022-04-12 20:44:41 +0800

    feat(store): add hx2022MsgData

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,14 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    hx2022MsgData: {},
   },
   getters: {
   },
   mutations: {
+    changeHx2022MsgData(state, val) {
+      state.hx2022MsgData = val
+    },
   },
   actions: {
   },
