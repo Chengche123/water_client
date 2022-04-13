196556fc763dca8feca56133281167a3dda64c36
佘曾伟 <chengche245@163.com>
2022-04-13 15:55:02 +0800

    feat(APP): 订阅 hx2022 变动 ws 消息

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,25 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "http://127.0.0.1:8000";
 
+const ws = new WebSocket("ws://localhost:8000/ws/hx2022-consumer/");
+
 export default {
   name: "App",
+  mounted() {
+    ws.onopen = function () {
+      ws.send(
+        JSON.stringify({
+          action: "subscribe_to_hx2022_activity",
+          request_id: new Date().getTime(),
+        })
+      );
+    };
+
+    ws.onmessage = (e) => {
+      const msgData = JSON.parse(e.data);
+      this.$store.commit("changeHx2022MsgData", msgData);
+    };
+  },
+  unmounted() {},
 };
 </script>
