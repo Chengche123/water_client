1a4a7e4798af52aeaa7390bc602e2943f46bac80
佘曾伟 <chengche245@163.com>
2022-04-14 11:52:03 +0800

    feat(SensorCard): 添加数据的动态显示

--- src/views/sensor/SensorCard.vue
+++ src/views/sensor/SensorCard.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="card opacity-75 text-start">
+  <div
+    class="card opacity-75 text-start pb-3"
+    :class="{ btn: isRunning }"
+    @click="handleClick"
+  >
+    <!-- 卡片内容 -->
     <div class="card-body p-0">
       <span class="opacity-100 card-title bg-danger text-white">{{
         sensorJson.sensortypename
       }}</span>
       <div class="ps-5">
+        <!-- 地址 -->
         <p class="mb-0">{{ sensorJson.address }}</p>
+        <!-- 状态 -->
         <div v-if="isRunning">
           <i class="bi bi-dot text-success"></i><span>传感器正常</span>
           <span class="ms-1 spinner-grow spinner-grow-sm text-success"></span>
@@ -14,21 +21,32 @@
           <i class="bi bi-dot text-danger"></i><span>传感器异常</span>
           <span class="ms-1 spinner-grow spinner-grow-sm text-danger"></span>
         </div>
-        <div class="row row-cols-3 mb-3 me-3 mt-1">
-          <template v-for="(value, inx) in dataValues" :key="value">
-            <span class="col-3 btn btn-info opacity-75 p-0">
-              {{ value }}
-            </span>
-            <span v-if="inx != dataValues.length - 1" class="col-1"></span>
-          </template>
+        <!-- 数据 -->
+        <div class="row">
+          <div class="col-8 mt-2">
+            <transition-group
+              enter-active-class="animate__animated animate__slideInRight items-active"
+              leave-active-class="animate__animated animate__slideOutRight position-absolute items-active"
+              enter-from-class="opacity-0"
+              leave-to-class="opacity-0"
+              move-class="items-move"
+            >
+              <div v-for="item in items" :key="`${item.udatetime}`">
+                <span class="bg-info px-1 opacity-75 mb-2">
+                  {{ item.value }} {{ sensorJson.measurename }}
+                </span>
+              </div>
+            </transition-group>
+          </div>
         </div>
-        <button @click="handleClick">测试</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "SensorCardView",
   props: {
@@ -37,32 +55,58 @@ export default {
       required: true,
     },
   },
+  async mounted() {
+    const dataPath = this.$store.state.hx2022Path;
+    const response = await axios.get(dataPath, {
+      params: {
+        code: this.sensorJson.code,
+        limit: 3,
+      },
+    });
+
+    let items = response?.data?.results;
+    if (!items) {
+      return;
+    }
+    items.reverse();
+    this.items = items;
+  },
   data() {
     return {
       isRunning: false,
-      dataValues: [12.34, 12.34, 12.34],
+      items: [],
     };
   },
   watch: {
+    // 接收 ws 消息
     "$store.state.hx2022MsgData": function (val) {
-      if (!val?.value) {
+      if (!val || val.code != this.sensorJson.code) {
         return;
       }
-      if (val.code == this.sensorJson.code) {
+
+      if (!this.isRunning) {
         this.isRunning = true;
       }
+
+      this.items = this.items.slice(1).concat(val);
     },
   },
   methods: {
-    handleClick() {
-      this.dataValues = this.dataValues.slice(1).concat(34.56);
-    },
+    handleClick() {},
   },
 };
 </script>
 
 <style scoped>
 .card:hover {
-  transform: scale(1.05);
+  box-shadow: 0 1rem 3rem rgba(0, 0, 0, 0.175) !important;
+}
+
+.items-move {
+  transition: all 1s ease;
+}
+
+.items-active {
+  transition: all 1s ease;
 }
 </style>
