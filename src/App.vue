90999279007a207eab301717bfb758a7eeb5268f
佘曾伟 <chengche245@163.com>
2022-04-21 16:05:05 +0800

    refactor(App): 将 home 页面放到单独的 SFC 中

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,27 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <nav
-        v-if="showNav"
-        class="
-          col-2
-          navbar
-          fixed-top
-          bottom-0
-          navbar-dark
-          bg-dark
-          align-content-start
-        "
-      >
-        <div class="navbar-brand text-info mb-4">矿井井下水文监测系统</div>
-        <ul class="navbar-nav flex-column">
-          <li class="nav-item">
-            <router-link
-              :to="{ name: 'sensor-home' }"
-              class="nav-link"
-              active-class="active"
-            >
-              <i class="iconfont icon">&#xe88e;</i>
-              实时监测
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link
-              :to="{ name: 'test' }"
-              class="nav-link"
-              active-class="active"
-            >
-              <i class="iconfont icon">&#xe606;</i>
-              调试
-            </router-link>
-          </li>
-        </ul>
-      </nav>
-
-      <div class="col-10 ms-auto px-4">
-        <!-- 在切换导航栏后不重新刷新页面 -->
-        <router-view v-slot="{ Component }">
-          <keep-alive>
-            <component :is="Component" />
-          </keep-alive>
-        </router-view>
-      </div>
-    </div>
-  </div>
+  <HomeView />
 </template>
 
 <script>
 import axios from "axios";
-
-// axios.defaults.baseURL =
-//   "https://www.fastmock.site/mock/820cb4d076cec84c7d445bb352e6c519/test";
+import HomeView from "./views/home/Home";
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "http://127.0.0.1:8000";
+
+export const API = {
+  hx2022: "/hx2022",
+  sensors: "/sensors",
+  login: "/users/login",
+  logout: "/users/logout",
+  user: "/user",
+};
 
 const ws = new WebSocket("ws://localhost:8000/ws/hx2022-consumer/");
 
 export default {
   name: "App",
+  components: { HomeView },
   data() {
     return {
       showNav: true,
@@ -88,44 +47,5 @@ export default {
     ws.close();
     console.log("app unmounted");
   },
-  watch: {
-    $route: function (val) {
-      if (val.name == "login") {
-        this.showNav = false;
-        return;
-      }
-
-      this.showNav = true;
-    },
-  },
 };
 </script>
-
-<style lang="scss" scoped>
-@import "@/style/utils.scss";
-
-.navbar .nav-link.active {
-  color: $primary;
-}
-
-.feather {
-  width: 16px;
-  height: 16px;
-  vertical-align: text-bottom;
-}
-
-.navbar .nav-link .icon {
-  margin-right: 4px;
-  color: $secondary;
-}
-
-.navbar .nav-link:hover .icon,
-.navbar .nav-link.active .icon {
-  color: inherit;
-}
-
-.sidebar-heading {
-  font-size: 0.75rem;
-  text-transform: uppercase;
-}
-</style>
