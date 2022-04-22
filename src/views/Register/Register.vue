f0b4226f4a6f88bed92c1516e13e09ddd642326e
佘曾伟 <chengche245@163.com>
2022-04-22 10:30:26 +0800

    feat(Register): 添加账号和确认密码的验证逻辑

---
+++ src/views/Register/Register.vue
@@ -0,0 +1,124 @@
+<template>
+  <div
+    class="
+      container-fluid
+      position-absolute
+      top-50
+      start-50
+      translate-middle
+      opacity-75
+    "
+  >
+    <ToastView
+      class="position-absolute top-50 start-50 translate-middle"
+      v-if="showToast"
+      :="toastParams"
+      style="z-index: 1"
+    />
+    <div class="mx-auto col-4">
+      <form
+        class="p-4 border rounded-3 bg-light was-validated"
+        @submit.prevent
+        ref="myForm"
+        novalidate
+      >
+        <div class="form-floating mb-3">
+          <input
+            v-model="username"
+            type="text"
+            class="form-control"
+            id="floatingUsername"
+            autocomplete="off"
+            pattern="^[a-zA-Z0-9_-]{4,16}$"
+            required
+          />
+          <label class="text-info" for="floatingUsername">账号</label>
+          <div class="invalid-feedback">
+            4到16位（字母，数字，下划线，减号）
+          </div>
+        </div>
+        <div class="form-floating mb-3">
+          <input
+            v-model="password"
+            type="password"
+            class="form-control"
+            id="floatingPassword"
+            autocomplete="off"
+            required
+          />
+          <label class="text-info" for="floatingPassword">密码</label>
+          <div class="invalid-feedback">密码不能为空</div>
+        </div>
+        <div class="form-floating mb-3">
+          <input
+            v-model="confirmPassword"
+            type="password"
+            class="form-control"
+            id="floatingConfirmPassword"
+            autocomplete="off"
+            ref="confirmPasswordInput"
+            required
+          />
+          <label class="text-info" for="floatingConfirmPassword"
+            >确认密码</label
+          >
+          <div class="invalid-feedback">
+            {{ $refs.confirmPasswordInput?.validationMessage }}
+          </div>
+        </div>
+        <button @click="handleRegister" class="w-100 btn btn-lg btn-primary">
+          注册
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import ToastView from "../../components/Toast.vue";
+
+export default {
+  name: "LoginView",
+  components: { ToastView },
+  data() {
+    return {
+      username: "",
+      password: "",
+      confirmPassword: "",
+      showToast: false,
+      toastParams: {
+        body: "",
+      },
+    };
+  },
+  mounted() {},
+  computed: {
+    passwords() {
+      const { password, confirmPassword } = this;
+      return {
+        password,
+        confirmPassword,
+      };
+    },
+  },
+  watch: {
+    passwords(newVal) {
+      const { password, confirmPassword } = newVal;
+      if (password != confirmPassword) {
+        this.$refs.confirmPasswordInput.setCustomValidity("两次密码不一致");
+        return;
+      }
+      this.$refs.confirmPasswordInput.setCustomValidity("");
+    },
+  },
+  methods: {
+    async handleRegister() {
+      if (!this.$refs.myForm.checkValidity()) {
+        return;
+      }
+
+      console.log("register");
+    },
+  },
+};
+</script>
